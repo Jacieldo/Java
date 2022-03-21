2ac42fd0d969bdbeb61868cf5c76aff10c7a605a
Jacieldo <jacieldo@hotmail.com>
2022-03-21 16:19:04 -0300

    UpData Java

---
+++ Atividades/src/Quinta.java
@@ -0,0 +1,13 @@
+public class Quinta {
+
+	public static void main(String[] args) {
+		//Tipo primitivo
+		int var1 = 5;
+		
+		Integer var2 = var1; //a partir da versão 9 não será mais necessário fazer referencias
+		
+		System.out.println(var2);
+
+	}
+
+}
