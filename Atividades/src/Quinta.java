0f847b60de8d9e00414a0d954d3571b3cf3d4658
Jacieldo Gonçalves <46200056+Jacieldo@users.noreply.github.com>
2022-03-22 19:58:24 -0300

    Melhorando a questão
    Atividade do módulo 5 do curso de java do mentorama. Estava incompleta. Correções feitas

--- Atividades/src/Quinta.java
+++ Atividades/src/Quinta.java
@@ -1,12 +1,23 @@
 public class Quinta {
-
+	
 	public static void main(String[] args) {
 		//Tipo primitivo
 		int var1 = 5;
+		float var2 = 3.14f;
+		double var3 = 6.989675;
+		byte var4 = 1;
 		
-		Integer var2 = var1; //a partir da versão 9 não será mais necessário fazer referencias
+		//a partir da versão 9 não será mais necessário fazer referencias
+		Integer varA = var1; 
+		Float varB = var2;
+		Double varC = var3;
+		Byte varD = var4;
 		
-		System.out.println(var2);
+		//Impressão dos valores
+		System.out.println("Integer - " + varA);
+		System.out.println("Float - " + varB);
+		System.out.println("Double - " + varC);
+		System.out.println("Byte - " + varD);
 
 	}
 
