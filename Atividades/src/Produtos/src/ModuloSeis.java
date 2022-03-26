ca1daacaebbdb08efc7baa4de566d8971cb72fd3
Jacieldo <jacieldo@hotmail.com>
2022-03-25 21:32:49 -0300

    Atividade Módulo 6 - Incremento
    Incrementação da atividade do módulo 6

--- Atividades/src/Produtos/src/ModuloSeis.java
+++ Atividades/src/Produtos/src/ModuloSeis.java
@@ -19,14 +19,34 @@ public class ModuloSeis {
 		System.out.print("Quantas aulas: ");
 		aulas = scanner.nextInt();
 		
-		String chamada[][] = new String[numAlunos][aulas];
+		String chamada[][] = new String[numAlunos][aulas+1];
 		
+		int i, j;
 		
-		for (int i = 0; i < numAlunos; i++) {
-			System.out.println("aluno: ");
-			chamada[i][0] = scanner.next();
-			
+		for (i = 0; i < numAlunos; i++) {
+			System.out.print(i+1 + "° aluno: ");
+			chamada[i][0] = scanner.next();			
 		}	
+		
+		System.out.println("p se estiver presente ou f para faltante");
+		
+		for (i = 0; i < aulas; i++) { //Cada Aula conta todos os alunos depois retete para outra aula
+			System.out.println("--- Presença da "+ (i+1) + " aula ---");
+			for (j = 0; j < numAlunos; j++) {
+				System.out.print(chamada[j][0]);
+				chamada[j][i+1] = scanner.next();				
+			}
+		}
+		
+		
+		//IMPRESSÃO DA MATRIZ
+		for (i = 0; i < numAlunos; i++) {
+			for (j = 0; j < (aulas + 1); j++) {
+				System.out.print(chamada[i][j] +" ");
+			}
+			System.out.println(" ");
+		}
+		
 		
 	}
 
